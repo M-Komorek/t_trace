9305549ab1f14bab2c66057640e7817f431a90f3
mkomorek <marcin.komorek@protonmail.com>
2025-07-23 12:12:28 +0200

    upgrade bash_hook

--- scripts/bash_hooks.sh
+++ scripts/bash_hooks.sh
@@ -1,15 +1,20 @@
 __t_trace_preexec() {
-  if [[ -z "$BASH_COMMAND" || "$BASH_COMMAND" == "$PROMPT_COMMAND" || "$BASH_COMMAND" =~ ^t_trace ]]; then
+  local this_command="$BASH_COMMAND"
+
+  # If the command is empty, part of the prompt machinery, or our own tool, ignore it.
+  # The regex checks if $this_command is a standalone command within PROMPT_COMMAND.
+  if [[ -z "$this_command" || "$PROMPT_COMMAND" =~ (^|;)${this_command}($|;) || "$this_command" =~ ^t_trace ]]; then
     return
   fi
 
-  command t_trace start --command "$BASH_COMMAND"
+  command t_trace start --command "$this_command"
 }
 
 __t_trace_precmd() {
   command t_trace end
 }
 
+trap - DEBUG
 trap '__t_trace_preexec' DEBUG
 
 if [[ -z "$PROMPT_COMMAND" ]]; then
